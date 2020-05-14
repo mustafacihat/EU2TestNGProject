08a00bde21611ddc1d53504e5927bf8c2abb552f
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-05-14 17:16:20 +0200

    Update configuration.properties, SingletonTest.java

--- src/test/java/com/cybertek/tests/day14_properties_driver_tests/SingletonTest.java
+++ src/test/java/com/cybertek/tests/day14_properties_driver_tests/SingletonTest.java
@@ -1,5 +1,8 @@
 package com.cybertek.tests.day14_properties_driver_tests;
 
+import com.cybertek.utilities.Driver;
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class SingletonTest {
@@ -11,5 +14,23 @@ public class SingletonTest {
 
         System.out.println("s1 = " + s1);
         System.out.println("s2 = " + s2);
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+
+
+        Driver.get().get("http://www.google.com");
+
+        Thread.sleep(2000);
+
+
+    }
+
+    @Test
+    public void test2(){
+
+        Driver.get().get("http://www.amazon.com");
+
     }
 }
